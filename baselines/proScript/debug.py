a3b2f8db378716a00bd3239f86b4b6f07a70c5a4
RishiHazra <rishixtreme@gmail.com>
2022-10-18 15:42:11 -0700

    baselines_all

--- baselines/proScript/debug.py
+++ baselines/proScript/debug.py
@@ -8,7 +8,7 @@ i2 = 'apple is sliced after heating'
 g2 = 'digraph graph {"Step 1 heat apple";"Step 2 slice apple";"Step 1 heat apple" -> "Step 2 slice apple";}'
 i3 = 'apple is picked, heated, then sliced'
 g3 = 'digraph graph {"Step 1 heat apple";"Step 2 slice apple";"Step 1 heat apple" -> "Step 2 slice apple";}'
-prompt_input = '{} => {} \n {} => g2 \n {} => '.format(i1, g1, i2, g2, i3)
+prompt_input = '{} => {} \n {} => {} \n {} => '.format(i1, g1, i2, g2, i3)
 prompt_ids = tokenizer(prompt_input, return_tensors="pt").input_ids
 outputs = t5_model.generate(prompt_ids)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
